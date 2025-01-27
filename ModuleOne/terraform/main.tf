b62597a58613e3d8898079d0fd4c2ba8ccb0e543
Chase-Lane <32445614+Chase-Lane@users.noreply.github.com>
2025-01-27 22:26:39 +0000

    adding tf variables file

--- ModuleOne/terraform/main.tf
+++ ModuleOne/terraform/main.tf
@@ -8,14 +8,14 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("./keys/my-creds.json")
-  project = "direct-tribute-447716-v7"
-  region  = "us-central1"
+  credentials = file(var.credentials)
+  project     = "direct-tribute-447716-v7"
+  region      = "us-central1"
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "direct-tribute-447716-terra-bucket"
-  location      = "US"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -26,4 +26,10 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
